fb43769693ddcfdfb80ab71c40b79fc6c6cec00d
beatuano <beatuano@gmail.com>
2023-02-07 17:12:50 -0500

    removed defined but never used line

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import "./App.css";
 // components
+import SignIn from "./Components/SignIn";
 import Header from "./Components/Header";
 import Main from "./Components/Main";
-import SignIn from "./Components/SignIn";
 
 function App() {
   return (
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,5 +1,3 @@
-import SignIn from "./SignIn";
-
 function Header() {
   return (
     <>
