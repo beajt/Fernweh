93f76a28d25e72ead9eb3297b073f2f2146afe78
beatuano <beatuano@gmail.com>
2023-03-31 00:59:37 -0400

    anon auth

--- src/Components/SignIn.js
+++ src/Components/SignIn.js
@@ -1,42 +1,58 @@
 import React, { useEffect, useState } from "react";
 import { auth, provider } from "./firebase";
-import { signInWithPopup, signOut, getAuth, signInAnonymously } from "firebase/auth";
-
-const authAnon = getAuth();
-signInAnonymously(authAnon)
-  .then(() => {
-    // signed in
-  });
-  // .catch((error) => {
-  //   const errorCode = error.code;
-  //   const errorMessage = error.message;
-  // });
+import {
+  signInWithPopup,
+  signOut,
+  getAuth,
+  signInAnonymously,
+} from "firebase/auth";
 
 function SignIn() {
   const [value, setValue] = useState("");
+  const [isAnonymous, setIsAnonymous] = useState(false);
+
+  useEffect(() => {
+    const authAnon = getAuth();
+    signInAnonymously(authAnon)
+      .then(() => {
+        setIsAnonymous(true);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+      });
+
+    setValue(localStorage.getItem("email"));
+  }, []);
+
   const handleClick = () => {
     signInWithPopup(auth, provider).then((data) => {
       setValue(data.user.email);
+      setIsAnonymous(false);
       localStorage.setItem("email", data.user.email);
     });
   };
 
-  useEffect(() => {
-    setValue(localStorage.getItem("email"));
-  }, []);
+  const handleAnonClick = () => {
+    setIsAnonymous(true);
+  };
 
   const logout = () => {
     signOut(auth);
     localStorage.clear();
+    setIsAnonymous(true);
     window.location.reload();
   };
 
   return (
     <div className="signIn">
-      {value ? (
-        <button onClick={logout}>Log Out</button>
-      ) : (
+      {isAnonymous ? (
         <button onClick={handleClick}>Sign In With Google</button>
+      ) : (
+        <button onClick={logout}>Log Out</button>
+      )}
+      {isAnonymous && (
+        <button className="hide" onClick={handleAnonClick}></button>
       )}
     </div>
   );
